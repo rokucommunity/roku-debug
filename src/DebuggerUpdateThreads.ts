df071a98365e003e0f9c4c99c7bab4c21c89c038
Christopher Dwyer-Perkins <cdwyerperkins@fubo.tv>
2020-02-25 16:30:33 -0400

    Updated the DebuggerUpdateThreads class to use the BufferReader.

--- src/DebuggerUpdateThreads.ts
+++ src/DebuggerUpdateThreads.ts
@@ -1,3 +1,5 @@
+import { BufferReader } from './BufferReader';
+
 const STOP_REASONS = {
   0: 'UNDEFINED',
   1: 'NOT_STOPPED',
@@ -35,27 +37,32 @@ class DebuggerUpdateThreads {
 
   constructor(buffer: Buffer) {
     if (buffer.byteLength >= 12) {
-      this.requestId = buffer.readUInt32LE(0);
-      if (this.requestId === 0) {
-        this.errorCode = ERROR_CODES[buffer.readUInt32LE(4)];
-        this.updateType = UPDATE_TYPES[buffer.readUInt32LE(8)];
+      try {
+        let bufferReader = new BufferReader(buffer);
+        this.requestId = bufferReader.readUInt32LE();
+        if (this.requestId === 0) {
+          this.errorCode = ERROR_CODES[bufferReader.readUInt32LE()];
+          this.updateType = UPDATE_TYPES[bufferReader.readUInt32LE()];
 
-        let threadsUpdate: ThreadsStopped | ThreadAttached;
-        if (this.updateType === 'ALL_THREADS_STOPPED') {
-          threadsUpdate = new ThreadsStopped(buffer, 12);
-          if (threadsUpdate.success) {
-            this.data = threadsUpdate;
-            this.byteLength = threadsUpdate.byteLength;
-            this.success = true;
-          }
-        } else if (this.updateType === 'THREAD_ATTACHED') {
-          threadsUpdate = new ThreadAttached(buffer, 12);
-          if (threadsUpdate.success) {
-            this.data = threadsUpdate;
-            this.byteLength = threadsUpdate.byteLength;
-            this.success = true;
+          let threadsUpdate: ThreadsStopped | ThreadAttached;
+          if (this.updateType === 'ALL_THREADS_STOPPED') {
+            threadsUpdate = new ThreadsStopped(bufferReader, 12);
+            if (threadsUpdate.success) {
+              this.data = threadsUpdate;
+              this.byteLength = bufferReader.offset;
+              this.success = true;
+            }
+          } else if (this.updateType === 'THREAD_ATTACHED') {
+            threadsUpdate = new ThreadAttached(bufferReader, 12);
+            if (threadsUpdate.success) {
+              this.data = threadsUpdate;
+              this.byteLength = bufferReader.byteLength;
+              this.success = true;
+            }
           }
         }
+      } catch (error) {
+        // Can't be parsed
       }
     }
   }
@@ -63,70 +70,36 @@ class DebuggerUpdateThreads {
 
 class ThreadsStopped {
   public success = false;
-  public byteLength = 0;
 
   // response fields
   public primaryThreadIndex = -1;
   public stopReason = -1;
   public stopReasonDetail: string;
 
-  constructor(buffer: Buffer, offset: number) {
-    if (buffer.byteLength >= offset + 6) {
-      this.primaryThreadIndex = buffer.readInt32LE(0 + offset);
-      this.stopReason = STOP_REASONS[buffer.readUInt8(4 + offset)];
-
-      let completeStopReason = false;
-      let stopReasonDetail: string;
-      let byteLength = offset + 5;
-      for (byteLength; byteLength <= buffer.length; byteLength ++) {
-        stopReasonDetail = buffer.toString('utf8', offset + 5, byteLength);
-
-        if (stopReasonDetail.endsWith('\0')) {
-          completeStopReason = true;
-          break;
-        }
-      }
-
-      if (completeStopReason) {
-        this.stopReasonDetail = stopReasonDetail;
-        this.byteLength = byteLength;
-        this.success = true;
-      }
+  constructor(bufferReader: BufferReader, offset: number) {
+    if (bufferReader.byteLength >= offset + 6) {
+      this.primaryThreadIndex = bufferReader.readInt32LE();
+      this.stopReason = STOP_REASONS[bufferReader.readUInt8()];
+      this.stopReasonDetail = bufferReader.readNTString();
+      this.success = true;
     }
   }
 }
 
 class ThreadAttached {
   public success = false;
-  public byteLength = 0;
 
   // response fields
   public threadIndex = -1;
   public stopReason = -1;
   public stopReasonDetail: string;
 
-  constructor(buffer: Buffer, offset: number) {
-    if (buffer.byteLength >= offset + 6) {
-      this.threadIndex = buffer.readInt32LE(0 + offset);
-      this.stopReason = STOP_REASONS[buffer.readUInt8(4 + offset)];
-
-      let completeStopReason = false;
-      let stopReasonDetail: string;
-      let byteLength = offset + 5;
-      for (byteLength; byteLength <= buffer.length; byteLength ++) {
-        stopReasonDetail = buffer.toString('utf8', offset + 5, byteLength);
-
-        if (stopReasonDetail.endsWith('\0')) {
-          completeStopReason = true;
-          break;
-        }
-      }
-
-      if (completeStopReason) {
-        this.stopReasonDetail = stopReasonDetail;
-        this.byteLength = byteLength;
-        this.success = true;
-      }
+  constructor(bufferReader: BufferReader, offset: number) {
+    if (bufferReader.byteLength >= offset + 6) {
+      this.threadIndex = bufferReader.readInt32LE();
+      this.stopReason = STOP_REASONS[bufferReader.readUInt8()];
+      this.stopReasonDetail = bufferReader.readNTString();
+      this.success = true;
     }
   }
 }
