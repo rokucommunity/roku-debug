8db61c502e2ca60ca59c38c6b0ec00e694f4df74
Christopher Dwyer-Perkins <chris@inverted-solutions.com>
2021-04-13 21:40:22 -0300

    Added the ChanperTracker class

---
+++ src/ChanperfTracker.spec.ts
@@ -0,0 +1,397 @@
+import * as sinon from 'sinon';
+import { assert, expect } from 'chai';
+import { ChanperfHistory, ChanperfTracker } from './ChanperfTracker';
+
+describe.only('BrightScriptFileUtils ', () => {
+    let chanperfTracker: ChanperfTracker;
+    let chanperfTrackerMock;
+    let logString: string;
+    let expectedHistory: ChanperfHistory;
+
+    beforeEach(() => {
+        chanperfTracker = new ChanperfTracker();
+        chanperfTrackerMock = sinon.mock(chanperfTracker);
+
+        // regex and examples also available at: https://regex101.com/r/AuQOxY/1
+        logString = `channel: Start
+            channel: mem=61560KiB{anon=36428,file=24884,shared=248},%cpu=13{user=10,sys=3}
+            channel: mem=65992KiB{anon=40852,file=24892,shared=248},%cpu=21{user=19,sys=2}
+            channel: mem=71836KiB{anon=46696,file=24892,shared=248},%cpu=30{user=25,sys=4}
+            channel: mem=71056KiB{anon=45916,file=24892,shared=248},%cpu=2{user=2,sys=0}
+            channel: mem=71056KiB{anon=45916,file=24892,shared=248},%cpu=0{user=0,sys=0}
+            channel: mem=71056KiB{anon=45916,file=24892,shared=248},%cpu=0{user=0,sys=0}
+            channel: mem=71056KiB{anon=45916,file=24892,shared=248},%cpu=0{user=0,sys=0}
+            channel: mem=71056KiB{anon=45916,file=24892,shared=248},%cpu=0{user=0,sys=0}
+            channel: mem=71056KiB{anon=45916,file=24892,shared=248},%cpu=0{user=0,sys=0}
+            channel: mem=71056KiB{anon=45916,file=24892,shared=248},%cpu=0{user=0,sys=0}
+            channel: mem=71056KiB{anon=45916,file=24892,shared=248},%cpu=0{user=0,sys=0}
+            channel: mem=71060KiB{anon=45916,file=24896,shared=248},%cpu=10{user=8,sys=2}
+            channel: mem=71056KiB{anon=45916,file=24896,shared=244},%cpu=4{user=2,sys=2}
+            channel: mem=71060KiB{anon=45920,file=24896,shared=244},%cpu=12{user=11,sys=1}
+            channel: mem=71220KiB{anon=46068,file=24904,shared=248},%cpu=20{user=17,sys=3}
+            Starting data processing
+            channel: mem=71220KiB{anon=46068,file=24904,shared=248},%cpu=0{user=0,sys=0}
+            channel: mem=71220KiB{anon=46068,file=24904,shared=248},%cpu=0{user=0,sys=0}
+            channel: mem=71220KiB{anon=46068,file=24904,shared=248},%cpu=0{user=0,sys=0}
+            channel: mem=71220KiB{anon=46068,file=24904,shared=248},%cpu=0{user=0,sys=0}
+            channel: mem=71220KiB{anon=46068,file=24904,shared=248},%cpu=0{user=0,sys=0}
+            channel: mem=71220KiB{anon=46068,file=24904,shared=248},%cpu=0{user=0,sys=0}
+            channel: mem=71220KiB{anon=46068,file=24904,shared=248},%cpu=0{user=0,sys=0}
+            channel: mem=71220KiB{anon=46068,file=24904,shared=248},%cpu=0{user=0,sys=0}
+            channel: mem=71232KiB{anon=46068,file=24916,shared=248},%cpu=1{user=1,sys=0}
+            channel: mem=71228KiB{anon=46068,file=24916,shared=244},%cpu=3{user=3,sys=0}
+            channel: mem=71676KiB{anon=46324,file=25104,shared=248},%cpu=3{user=2,sys=1}
+            Data processing completed
+            channel: mem=71220KiB{anon=46068,file=24904,shared=248},%cpu=0{user=0,sys=0}
+            channel: mem=71220KiB{anon=46068,file=24904,shared=248},%cpu=0{user=0,sys=0}
+            channel: mem=71220KiB{anon=46068,file=24904,shared=248},%cpu=0{user=0,sys=0}
+            channel: mem=71220KiB{anon=46068,file=24904,shared=248},%cpu=0{user=0,sys=0}
+            channel: mem=71220KiB{anon=46068,file=24904,shared=248},%cpu=0{user=0,sys=0}
+            channel: mem=71220KiB{anon=46068,file=24904,shared=248},%cpu=0{user=0,sys=0}
+            channel: mem=71220KiB{anon=46068,file=24904,shared=248},%cpu=0{user=0,sys=0}
+            channel: mem=71220KiB{anon=46068,file=24904,shared=248},%cpu=0{user=0,sys=0}
+            channel: mem=71232KiB{anon=46068,file=24916,shared=248},%cpu=1{user=1,sys=0}
+            channel: mem=71228KiB{anon=46068,file=24916,shared=244},%cpu=3{user=3,sys=0}
+            channel: mem=71676KiB{anon=46324,file=25104,shared=248},%cpu=3{user=2,sys=1}
+        `.replace(/ {4}/g, '');
+
+        expectedHistory = {
+            totalMemKib: 71676,
+            anonMemKib: 46324,
+            fileMemKib: 25104,
+            sharedMemKib: 248,
+            totalCpuUsage: 3,
+            userCpuUsage: 2,
+            sysCpuUsage: 1,
+            dataSet: {
+                totalMemKib: [
+                    61560,
+                    65992,
+                    71836,
+                    71056,
+                    71056,
+                    71056,
+                    71056,
+                    71056,
+                    71056,
+                    71056,
+                    71056,
+                    71060,
+                    71056,
+                    71060,
+                    71220,
+                    71220,
+                    71220,
+                    71220,
+                    71220,
+                    71220,
+                    71220,
+                    71220,
+                    71220,
+                    71232,
+                    71228,
+                    71676,
+                    71220,
+                    71220,
+                    71220,
+                    71220,
+                    71220,
+                    71220,
+                    71220,
+                    71220,
+                    71232,
+                    71228,
+                    71676
+                ],
+                anonMemKib: [
+                    36428,
+                    40852,
+                    46696,
+                    45916,
+                    45916,
+                    45916,
+                    45916,
+                    45916,
+                    45916,
+                    45916,
+                    45916,
+                    45916,
+                    45916,
+                    45920,
+                    46068,
+                    46068,
+                    46068,
+                    46068,
+                    46068,
+                    46068,
+                    46068,
+                    46068,
+                    46068,
+                    46068,
+                    46068,
+                    46324,
+                    46068,
+                    46068,
+                    46068,
+                    46068,
+                    46068,
+                    46068,
+                    46068,
+                    46068,
+                    46068,
+                    46068,
+                    46324
+                ],
+                fileMemKib: [
+                    24884,
+                    24892,
+                    24892,
+                    24892,
+                    24892,
+                    24892,
+                    24892,
+                    24892,
+                    24892,
+                    24892,
+                    24892,
+                    24896,
+                    24896,
+                    24896,
+                    24904,
+                    24904,
+                    24904,
+                    24904,
+                    24904,
+                    24904,
+                    24904,
+                    24904,
+                    24904,
+                    24916,
+                    24916,
+                    25104,
+                    24904,
+                    24904,
+                    24904,
+                    24904,
+                    24904,
+                    24904,
+                    24904,
+                    24904,
+                    24916,
+                    24916,
+                    25104
+                ],
+                sharedMemKib: [
+                    248,
+                    248,
+                    248,
+                    248,
+                    248,
+                    248,
+                    248,
+                    248,
+                    248,
+                    248,
+                    248,
+                    248,
+                    244,
+                    244,
+                    248,
+                    248,
+                    248,
+                    248,
+                    248,
+                    248,
+                    248,
+                    248,
+                    248,
+                    248,
+                    244,
+                    248,
+                    248,
+                    248,
+                    248,
+                    248,
+                    248,
+                    248,
+                    248,
+                    248,
+                    248,
+                    244,
+                    248
+                ],
+                totalCpuUsage: [
+                    13,
+                    21,
+                    30,
+                    2,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    10,
+                    4,
+                    12,
+                    20,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    1,
+                    3,
+                    3,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    1,
+                    3,
+                    3
+                ],
+                userCpuUsage: [
+                    10,
+                    19,
+                    25,
+                    2,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    8,
+                    2,
+                    11,
+                    17,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    1,
+                    3,
+                    2,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    1,
+                    3,
+                    2
+                ],
+                sysCpuUsage: [
+                    3,
+                    2,
+                    4,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    2,
+                    2,
+                    1,
+                    3,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    1,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    1
+                ]
+            }
+        };
+
+    });
+
+    afterEach(() => {
+        chanperfTrackerMock.restore();
+    });
+
+    describe('processLogLine ', () => {
+        it('filters out all chanperf log lines', async () => {
+            chanperfTrackerMock.expects('emit').withArgs('chanperf-event').once();
+            let expected = `channel: Start\nStarting data processing\nData processing completed\n`;
+            assert.equal(await chanperfTracker.processLogLine(logString), expected);
+            assert.deepEqual(chanperfTracker.getChanperfHistory, expectedHistory);
+            chanperfTrackerMock.verify();
+        });
+
+        it('does not filter out chanperf log lines', async () => {
+            chanperfTrackerMock.expects('emit').withArgs('chanperf-event').once();
+            chanperfTracker.setConsoleOutput('full');
+            assert.equal(await chanperfTracker.processLogLine(logString), logString);
+            assert.deepEqual(chanperfTracker.getChanperfHistory, expectedHistory);
+            chanperfTrackerMock.verify();
+        });
+    });
+
+    describe('clearChanperfHistory', () => {
+        it('to reset the history data', async () => {
+            chanperfTrackerMock.expects('emit').withArgs('chanperf-event').twice();
+            let expected = `channel: Start\nStarting data processing\nData processing completed\n`;
+            assert.equal(await chanperfTracker.processLogLine(logString), expected);
+            assert.deepEqual(chanperfTracker.getChanperfHistory, expectedHistory);
+
+            chanperfTracker.clearChanperfHistory();
+
+            assert.deepEqual(chanperfTracker.getChanperfHistory, {
+                totalMemKib: 0,
+                anonMemKib: 0,
+                fileMemKib: 0,
+                sharedMemKib: 0,
+                totalCpuUsage: 0,
+                userCpuUsage: 0,
+                sysCpuUsage: 0,
+                dataSet: {
+                    totalMemKib: [],
+                    anonMemKib: [],
+                    fileMemKib: [],
+                    sharedMemKib: [],
+                    totalCpuUsage: [],
+                    userCpuUsage: [],
+                    sysCpuUsage: []
+                }
+            });
+            chanperfTrackerMock.verify();
+        });
+    });
+});
---
+++ src/ChanperfTracker.ts
@@ -0,0 +1,141 @@
+import { EventEmitter } from 'events';
+
+export class ChanperfTracker {
+    constructor() {
+        this.emitter = new EventEmitter();
+        this.filterOutLogs = true;
+        this.chanperfHistory = this.createNewChanperfHistory();
+    }
+
+    private emitter: EventEmitter;
+    private filterOutLogs: boolean;
+    private chanperfHistory: ChanperfHistory;
+
+    public on(eventname: 'chanperf-event', handler: (output: ChanperfHistory) => void);
+    public on(eventName: string, handler: (payload: any) => void) {
+        this.emitter.on(eventName, handler);
+        return () => {
+            if (this.emitter !== undefined) {
+                this.emitter.removeListener(eventName, handler);
+            }
+        };
+    }
+
+    private emit(eventName: 'chanperf-event', data?) {
+        this.emitter.emit(eventName, data);
+    }
+
+    public get getChanperfHistory(): ChanperfHistory {
+        return this.chanperfHistory;
+    }
+
+    /**
+     * Used to set wether the chanperf should be filtered from the console output
+     * @param outputLevel the consoleOutput from the launch config
+     */
+    public setConsoleOutput(outputLevel: string) {
+        this.filterOutLogs = !(outputLevel === 'full');
+    }
+
+    /**
+     * Clears the current chanperf history
+     */
+    public clearChanperfHistory() {
+        this.chanperfHistory = this.createNewChanperfHistory();
+        this.emit('chanperf-event', this.chanperfHistory);
+    }
+
+    /**
+     * Takes the debug output from the device and parses it for any chanperf information.
+     * Also if consoleOutput was not set to 'full' then any chanperf output will be filtered from the output.
+     * @param logLine
+     * @returns The debug output after parsing
+     */
+    public async processLogLine(logLine: string): Promise<string> {
+        let dataChanged = false;
+        let lines = logLine.split('\n');
+        let normalOutput = '';
+
+        for (let line of lines) {
+            let match = /channel: *mem=([0-9]+)kib{[a-z]+=([0-9]+),[a-z]+=([0-9]+),[a-z]+=([0-9]+)},\%cpu=([0-9]+){[a-z]+=([0-9]+),[a-z]+=([0-9]+)}/gmi.exec(line);
+            // see the following for an explanation for this regex: https://regex101.com/r/AuQOxY/1
+            if (match) {
+                let [fullMatch, totalMemKib, anonMemKib, fileMemKib, sharedMemKib, totalCpuUsage, userCpuUsage, sysCpuUsage] = match;
+
+                this.chanperfHistory.totalMemKib = parseInt(totalMemKib)
+                this.chanperfHistory.anonMemKib = parseInt(anonMemKib)
+                this.chanperfHistory.fileMemKib = parseInt(fileMemKib)
+                this.chanperfHistory.sharedMemKib = parseInt(sharedMemKib)
+                this.chanperfHistory.totalCpuUsage = parseInt(totalCpuUsage)
+                this.chanperfHistory.userCpuUsage = parseInt(userCpuUsage)
+                this.chanperfHistory.sysCpuUsage = parseInt(sysCpuUsage)
+
+                this.chanperfHistory.dataSet.totalMemKib.push(parseInt(totalMemKib))
+                this.chanperfHistory.dataSet.anonMemKib.push(parseInt(anonMemKib))
+                this.chanperfHistory.dataSet.fileMemKib.push(parseInt(fileMemKib))
+                this.chanperfHistory.dataSet.sharedMemKib.push(parseInt(sharedMemKib))
+                this.chanperfHistory.dataSet.totalCpuUsage.push(parseInt(totalCpuUsage))
+                this.chanperfHistory.dataSet.userCpuUsage.push(parseInt(userCpuUsage))
+                this.chanperfHistory.dataSet.sysCpuUsage.push(parseInt(sysCpuUsage))
+
+                if (!this.filterOutLogs) {
+                    normalOutput += line + '\n';
+                }
+                dataChanged = true;
+            } else if (line) {
+                normalOutput += line + '\n';
+            }
+        }
+
+        if (dataChanged) {
+            this.emit('chanperf-event', this.chanperfHistory);
+        }
+
+        return normalOutput;
+    }
+
+    /**
+     * Helper function used to create a new ChanperfHistory object with default values
+     */
+    private createNewChanperfHistory(): ChanperfHistory {
+        return {
+            totalMemKib: 0,
+            anonMemKib: 0,
+            fileMemKib: 0,
+            sharedMemKib: 0,
+            totalCpuUsage: 0,
+            userCpuUsage: 0,
+            sysCpuUsage: 0,
+            dataSet: {
+                totalMemKib: [],
+                anonMemKib: [],
+                fileMemKib: [],
+                sharedMemKib: [],
+                totalCpuUsage: [],
+                userCpuUsage: [],
+                sysCpuUsage: []
+            }
+        };
+    }
+}
+
+export interface ChanperfHistory {
+    totalMemKib: number;
+    anonMemKib: number;
+    fileMemKib: number;
+    sharedMemKib: number;
+    totalCpuUsage: number;
+    userCpuUsage: number;
+    sysCpuUsage: number;
+    dataSet: ChanperfDataSet;
+}
+
+interface ChanperfDataSet {
+    totalMemKib: Array<number>;
+    anonMemKib: Array<number>;
+    fileMemKib: Array<number>;
+    sharedMemKib: Array<number>;
+    totalCpuUsage: Array<number>;
+    userCpuUsage: Array<number>;
+    sysCpuUsage: Array<number>;
+}
