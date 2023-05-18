bdfc927cfd2e3bbee1e732f495174190f887d79b
Bronley Plumb <bronley@gmail.com>
2023-05-17 22:52:52 -0400

    Fix crash by using postman-request (#151)

--- src/debugSession/BrightScriptDebugSession.ts
+++ src/debugSession/BrightScriptDebugSession.ts
@@ -1,7 +1,7 @@
 import * as fsExtra from 'fs-extra';
 import { orderBy } from 'natural-orderby';
 import * as path from 'path';
-import * as request from 'request';
+import * as request from 'postman-request';
 import { rokuDeploy, CompileError } from 'roku-deploy';
 import type { RokuDeploy, RokuDeployOptions } from 'roku-deploy';
 import {
