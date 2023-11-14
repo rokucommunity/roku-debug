dd7fee537e469429ef6d33c73693784c565a8d78
Bronley Plumb <bronley@gmail.com>
2023-11-14 10:00:40 -0500

    Add cli flag to run dap as standalone process

---
+++ src/cli.ts
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+import * as yargs from 'yargs';
+import { BrightScriptDebugSession } from './debugSession/BrightScriptDebugSession';
+
+let options = yargs
+    .usage('$0', 'BrighterScript, a superset of Roku\'s BrightScript language')
+    .help('help', 'View help information about this tool.')
+    .option('dap', { type: 'boolean', defaultDescription: 'false', description: 'Run roku-debug as a standalone debug-adapter-protocol process, communicating over STDIO' })
+    .argv;
+
+(function main() {
+    if (options.dap) {
+        BrightScriptDebugSession.run(BrightScriptDebugSession);
+    } else {
+        throw new Error('Not supported');
+    }
+}());
