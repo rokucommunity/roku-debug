8e7c450460162dc67c7e1eb1e9666424f87a39d9
Christian Holbrook <cholbrook@fubo.tv>
2024-11-21 11:20:33 -0700

    Change the gate keeping logic to a nested if block instead of an early out if check

--- src/adapters/InsertVariableHelper.ts
+++ src/adapters/InsertVariableHelper.ts
@@ -3,33 +3,32 @@ import { KeyType } from './DebugProtocolAdapter';
 import type { DebugProtocolAdapter, EvaluateContainer } from './DebugProtocolAdapter';
 
 export function insertCustomVariablesHelpers(adapter: DebugProtocolAdapter, expression: string, container: EvaluateContainer) {
-    if (!semver.satisfies(adapter?.activeProtocolVersion, '<3.3.0')) {
-        return;
-    }
-    if (container?.value?.startsWith('roSGNode')) {
-        let nodeChildren = <EvaluateContainer>{
-            name: '[[children]]',
-            type: 'roArray',
-            highLevelType: 'array',
-            keyType: KeyType.integer,
-            presentationHint: 'virtual',
-            evaluateName: `${expression}.getChildren(-1, 0)`,
-            children: []
-        };
-        container.children.push(nodeChildren);
-    }
-    if (container.elementCount > 0 || container.type === 'Array') {
-        let nodeCount = <EvaluateContainer>{
-            name: '[[count]]',
-            evaluateName: container.elementCount.toString(),
-            type: 'number',
-            highLevelType: undefined,
-            keyType: undefined,
-            presentationHint: 'virtual',
-            value: container.elementCount.toString(),
-            elementCount: undefined,
-            children: []
-        };
-        container.children.push(nodeCount);
+    if (semver.satisfies(adapter?.activeProtocolVersion, '<3.3.0')) {
+        if (container?.value?.startsWith('roSGNode')) {
+            let nodeChildren = <EvaluateContainer>{
+                name: '[[children]]',
+                type: 'roArray',
+                highLevelType: 'array',
+                keyType: KeyType.integer,
+                presentationHint: 'virtual',
+                evaluateName: `${expression}.getChildren(-1, 0)`,
+                children: []
+            };
+            container.children.push(nodeChildren);
+        }
+        if (container.elementCount > 0 || container.type === 'Array') {
+            let nodeCount = <EvaluateContainer>{
+                name: '[[count]]',
+                evaluateName: container.elementCount.toString(),
+                type: 'number',
+                highLevelType: undefined,
+                keyType: undefined,
+                presentationHint: 'virtual',
+                value: container.elementCount.toString(),
+                elementCount: undefined,
+                children: []
+            };
+            container.children.push(nodeCount);
+        }
     }
 }
