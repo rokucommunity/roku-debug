4d69c3ea2cc9d893f9869362e563041286ecd6ba
Bronley Plumb <bronley@gmail.com>
2024-12-02 08:47:39 -0500

    Make the cli test longer

--- src/cli.spec.ts
+++ src/cli.spec.ts
@@ -3,7 +3,7 @@ import { execSync } from 'child_process';
 
 describe('cli', () => {
     it('runs without crashing and includes summary', function() {
-        this.timeout(10_000);
+        this.timeout(30_000);
         expect(
             execSync('npx ts-node src/cli.ts --help').toString()
         ).to.include('roku-debug, support for debugging Roku devices via telnet or debug protocol');
