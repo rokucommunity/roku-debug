f6ebb828b8a35a2cf763a613780e89fef92a4b22
Christopher Dwyer-Perkins <chris@inverted-solutions.com>
2025-01-24 15:17:01 -0400

    Added roDeviceInfo virtual variables

--- src/adapters/customVariableUtils.ts
+++ src/adapters/customVariableUtils.ts
@@ -17,7 +17,6 @@ export async function insertCustomVariables(adapter: DebugProtocolAdapter, expre
                 pushCustomVariableToContainer(container, {
                     name: '$children',
                     type: VariableType.Array,
-                    highLevelType: HighLevelType.array,
                     keyType: KeyType.integer,
                     presentationHint: 'virtual',
                     evaluateName: `${expression}.getChildren(-1, 0)`,
@@ -37,7 +36,6 @@ export async function insertCustomVariables(adapter: DebugProtocolAdapter, expre
                 pushCustomVariableToContainer(container, {
                     name: '$threadinfo',
                     type: VariableType.AssociativeArray,
-                    highLevelType: HighLevelType.object,
                     keyType: KeyType.string,
                     presentationHint: 'virtual',
                     evaluateName: `${expression}.threadInfo()`,
@@ -280,6 +278,428 @@ export async function insertCustomVariables(adapter: DebugProtocolAdapter, expre
                 children: []
             });
         }
+
+        if (container.type === 'roDeviceInfo') {
+            pushCustomVariableToContainer(container, {
+                name: '$model',
+                type: VariableType.String,
+                presentationHint: 'virtual',
+                evaluateName: `${expression}.GetModel()`,
+                lazy: true,
+                value: '',
+                children: []
+            });
+
+            pushCustomVariableToContainer(container, {
+                name: '$modeldisplayname',
+                type: VariableType.String,
+                presentationHint: 'virtual',
+                evaluateName: `${expression}.GetModelDisplayName()`,
+                lazy: true,
+                value: '',
+                children: []
+            });
+
+            pushCustomVariableToContainer(container, {
+                name: '$modeltype',
+                type: VariableType.String,
+                presentationHint: 'virtual',
+                evaluateName: `${expression}.GetModelType()`,
+                lazy: true,
+                value: '',
+                children: []
+            });
+
+            pushCustomVariableToContainer(container, {
+                name: '$modeldetails',
+                type: VariableType.AssociativeArray,
+                presentationHint: 'virtual',
+                evaluateName: `${expression}.GetModelDetails()`,
+                lazy: true,
+                value: '',
+                children: []
+            });
+
+            pushCustomVariableToContainer(container, {
+                name: '$friendlyname',
+                type: VariableType.String,
+                presentationHint: 'virtual',
+                evaluateName: `${expression}.GetFriendlyName()`,
+                lazy: true,
+                value: '',
+                children: []
+            });
+
+            pushCustomVariableToContainer(container, {
+                name: '$osversion',
+                type: VariableType.AssociativeArray,
+                presentationHint: 'virtual',
+                evaluateName: `${expression}.GetOSVersion()`,
+                lazy: true,
+                value: '',
+                children: []
+            });
+
+            pushCustomVariableToContainer(container, {
+                name: '$rida',
+                type: VariableType.String,
+                presentationHint: 'virtual',
+                evaluateName: `${expression}.GetRIDA()`,
+                lazy: true,
+                value: '',
+                children: []
+            });
+
+            pushCustomVariableToContainer(container, {
+                name: '$ridadisabled',
+                type: VariableType.Boolean,
+                presentationHint: 'virtual',
+                evaluateName: `${expression}.IsRIDADisabled()`,
+                lazy: true,
+                value: '',
+                children: []
+            });
+
+            pushCustomVariableToContainer(container, {
+                name: '$channelclientid',
+                type: VariableType.String,
+                presentationHint: 'virtual',
+                evaluateName: `${expression}.GetChannelClientId()`,
+                lazy: true,
+                value: '',
+                children: []
+            });
+
+            pushCustomVariableToContainer(container, {
+                name: '$usercountrycode',
+                type: VariableType.String,
+                presentationHint: 'virtual',
+                evaluateName: `${expression}.GetUserCountryCode()`,
+                lazy: true,
+                value: '',
+                children: []
+            });
+
+            pushCustomVariableToContainer(container, {
+                name: '$randomuuid',
+                type: VariableType.String,
+                presentationHint: 'virtual',
+                evaluateName: `${expression}.GetRandomUUID()`,
+                lazy: true,
+                value: '',
+                children: []
+            });
+
+            pushCustomVariableToContainer(container, {
+                name: '$timezone',
+                type: VariableType.String,
+                presentationHint: 'virtual',
+                evaluateName: `${expression}.GetTimeZone()`,
+                lazy: true,
+                value: '',
+                children: []
+            });
+
+            pushCustomVariableToContainer(container, {
+                name: '$currentlocale',
+                type: VariableType.String,
+                presentationHint: 'virtual',
+                evaluateName: `${expression}.GetCurrentLocale()`,
+                lazy: true,
+                value: '',
+                children: []
+            });
+
+            pushCustomVariableToContainer(container, {
+                name: '$countrycode',
+                type: VariableType.String,
+                presentationHint: 'virtual',
+                evaluateName: `${expression}.GetCountryCode()`,
+                lazy: true,
+                value: '',
+                children: []
+            });
+
+            pushCustomVariableToContainer(container, {
+                name: '$preferredcaptionlanguage',
+                type: VariableType.String,
+                presentationHint: 'virtual',
+                evaluateName: `${expression}.GetPreferredCaptionLanguage()`,
+                lazy: true,
+                value: '',
+                children: []
+            });
+
+            pushCustomVariableToContainer(container, {
+                name: '$timesincelastkeypress',
+                type: VariableType.Integer,
+                presentationHint: 'virtual',
+                evaluateName: `${expression}.TimeSinceLastKeypress()`,
+                lazy: true,
+                value: '',
+                children: []
+            });
+
+            pushCustomVariableToContainer(container, {
+                name: '$drminfoex',
+                type: VariableType.AssociativeArray,
+                presentationHint: 'virtual',
+                evaluateName: `${expression}.GetDrmInfoEx()`,
+                lazy: true,
+                value: '',
+                children: []
+            });
+
+            pushCustomVariableToContainer(container, {
+                name: '$captionsmode',
+                type: VariableType.String,
+                presentationHint: 'virtual',
+                evaluateName: `${expression}.GetCaptionsMode()`,
+                lazy: true,
+                value: '',
+                children: []
+            });
+
+            pushCustomVariableToContainer(container, {
+                name: '$clockformat',
+                type: VariableType.String,
+                presentationHint: 'virtual',
+                evaluateName: `${expression}.GetClockFormat()`,
+                lazy: true,
+                value: '',
+                children: []
+            });
+
+            pushCustomVariableToContainer(container, {
+                name: '$clockvalid',
+                type: VariableType.Boolean,
+                presentationHint: 'virtual',
+                evaluateName: `${expression}.GetClockFormat()`,
+                lazy: true,
+                value: '',
+                children: []
+            });
+
+            pushCustomVariableToContainer(container, {
+                name: '$generalmemorylevel',
+                type: VariableType.Boolean,
+                presentationHint: 'virtual',
+                evaluateName: `${expression}.GetGeneralMemoryLevel()`,
+                lazy: true,
+                value: '',
+                children: []
+            });
+
+            pushCustomVariableToContainer(container, {
+                name: '$storedemomode',
+                type: VariableType.Boolean,
+                presentationHint: 'virtual',
+                evaluateName: `${expression}.IsStoreDemoMode()`,
+                lazy: true,
+                value: '',
+                children: []
+            });
+
+            pushCustomVariableToContainer(container, {
+                name: '$linkstatus',
+                type: VariableType.Boolean,
+                presentationHint: 'virtual',
+                evaluateName: `${expression}.GetLinkStatus()`,
+                lazy: true,
+                value: '',
+                children: []
+            });
+
+            pushCustomVariableToContainer(container, {
+                name: '$connectiontype',
+                type: VariableType.String,
+                presentationHint: 'virtual',
+                evaluateName: `${expression}.GetConnectionType()`,
+                lazy: true,
+                value: '',
+                children: []
+            });
+
+            pushCustomVariableToContainer(container, {
+                name: '$internetstatus',
+                type: VariableType.Boolean,
+                presentationHint: 'virtual',
+                evaluateName: `${expression}.GetInternetStatus()`,
+                lazy: true,
+                value: '',
+                children: []
+            });
+
+            pushCustomVariableToContainer(container, {
+                name: '$externalip',
+                type: VariableType.String,
+                presentationHint: 'virtual',
+                evaluateName: `${expression}.GetExternalIp()`,
+                lazy: true,
+                value: '',
+                children: []
+            });
+
+            pushCustomVariableToContainer(container, {
+                name: '$ipaddrs',
+                type: VariableType.AssociativeArray,
+                presentationHint: 'virtual',
+                evaluateName: `${expression}.GetIPAddrs()`,
+                lazy: true,
+                value: '',
+                children: []
+            });
+
+            pushCustomVariableToContainer(container, {
+                name: '$connectioninfo',
+                type: VariableType.AssociativeArray,
+                presentationHint: 'virtual',
+                evaluateName: `${expression}.GetConnectionInfo()`,
+                lazy: true,
+                value: '',
+                children: []
+            });
+
+            pushCustomVariableToContainer(container, {
+                name: '$displaytype',
+                type: VariableType.String,
+                presentationHint: 'virtual',
+                evaluateName: `${expression}.GetDisplayType()`,
+                lazy: true,
+                value: '',
+                children: []
+            });
+
+            pushCustomVariableToContainer(container, {
+                name: '$displaymode',
+                type: VariableType.String,
+                presentationHint: 'virtual',
+                evaluateName: `${expression}.GetDisplayMode()`,
+                lazy: true,
+                value: '',
+                children: []
+            });
+
+            pushCustomVariableToContainer(container, {
+                name: '$displayaspectratio',
+                type: VariableType.String,
+                presentationHint: 'virtual',
+                evaluateName: `${expression}.GetDisplayAspectRatio()`,
+                lazy: true,
+                value: '',
+                children: []
+            });
+
+            pushCustomVariableToContainer(container, {
+                name: '$displaysize',
+                type: VariableType.AssociativeArray,
+                presentationHint: 'virtual',
+                evaluateName: `${expression}.GetDisplaySize()`,
+                lazy: true,
+                value: '',
+                children: []
+            });
+
+            pushCustomVariableToContainer(container, {
+                name: '$videomode',
+                type: VariableType.String,
+                presentationHint: 'virtual',
+                evaluateName: `${expression}.GetVideoMode()`,
+                lazy: true,
+                value: '',
+                children: []
+            });
+
+            pushCustomVariableToContainer(container, {
+                name: '$displayproperties',
+                type: VariableType.AssociativeArray,
+                presentationHint: 'virtual',
+                evaluateName: `${expression}.GetDisplayProperties()`,
+                lazy: true,
+                value: '',
+                children: []
+            });
+
+            pushCustomVariableToContainer(container, {
+                name: '$supportedgraphicsresolutions',
+                type: VariableType.AssociativeArray,
+                presentationHint: 'virtual',
+                evaluateName: `${expression}.GetSupportedGraphicsResolutions()`,
+                lazy: true,
+                value: '',
+                children: []
+            });
+
+            pushCustomVariableToContainer(container, {
+                name: '$uiresolution',
+                type: VariableType.AssociativeArray,
+                presentationHint: 'virtual',
+                evaluateName: `${expression}.GetUIResolution()`,
+                lazy: true,
+                value: '',
+                children: []
+            });
+
+            pushCustomVariableToContainer(container, {
+                name: '$graphicsplatform',
+                type: VariableType.String,
+                presentationHint: 'virtual',
+                evaluateName: `${expression}.GetGraphicsPlatform()`,
+                lazy: true,
+                value: '',
+                children: []
+            });
+
+            pushCustomVariableToContainer(container, {
+                name: '$audiooutputchannel',
+                type: VariableType.String,
+                presentationHint: 'virtual',
+                evaluateName: `${expression}.GetAudioOutputChannel()`,
+                lazy: true,
+                value: '',
+                children: []
+            });
+
+            pushCustomVariableToContainer(container, {
+                name: '$passthrucodecactive',
+                type: VariableType.Boolean,
+                presentationHint: 'virtual',
+                evaluateName: `${expression}.IsPassthruCodecActive()`,
+                lazy: true,
+                value: '',
+                children: []
+            });
+
+            pushCustomVariableToContainer(container, {
+                name: '$soundeffectsvolume',
+                type: VariableType.Integer,
+                presentationHint: 'virtual',
+                evaluateName: `${expression}.GetSoundEffectsVolume()`,
+                lazy: true,
+                value: '',
+                children: []
+            });
+
+            pushCustomVariableToContainer(container, {
+                name: '$audioguideenabled',
+                type: VariableType.Boolean,
+                presentationHint: 'virtual',
+                evaluateName: `${expression}.IsAudioGuideEnabled()`,
+                lazy: true,
+                value: '',
+                children: []
+            });
+
+            pushCustomVariableToContainer(container, {
+                name: '$autoplayenabled',
+                type: VariableType.Boolean,
+                presentationHint: 'virtual',
+                evaluateName: `${expression}.IsAutoplayEnabled()`,
+                lazy: true,
+                value: '',
+                children: []
+            });
+        }
     } catch (e) {
         // Error inserting custom variables. We don't want to cause issues with real variables so just move on for now.
     }
@@ -291,7 +711,7 @@ export async function insertCustomVariables(adapter: DebugProtocolAdapter, expre
  */
 export function overrideKeyTypesForCustomVariables(adapter: DebugProtocolAdapter, container: EvaluateContainer) {
     if (!container.keyType) {
-        if (['roUrlTransfer', 'roDateTime'].includes(container.type)) {
+        if (['roUrlTransfer', 'roDateTime', 'roDeviceInfo'].includes(container.type)) {
             container.keyType = KeyType.string;
         }
     }
@@ -302,6 +722,15 @@ export function overrideKeyTypesForCustomVariables(adapter: DebugProtocolAdapter
  */
 function pushCustomVariableToContainer(container: EvaluateContainer, customVariable: EvaluateContainer) {
     if (!container.children.some(child => child.name === customVariable.name)) {
+
+        if (customVariable.type === VariableType.AssociativeArray) {
+            customVariable.highLevelType = HighLevelType.object;
+            customVariable.keyType = KeyType.string;
+        } else if (customVariable.type === VariableType.Array) {
+            customVariable.highLevelType = HighLevelType.array;
+            customVariable.keyType = KeyType.integer;
+        }
+
         container.children.push(customVariable);
     }
 }
