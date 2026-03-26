77e24e84c5a16f431494fd09180f074f634239a8
RokuSerhii <steplov@roku.com>
2026-03-26 18:00:42 +0200

    add handling of optional threadrequest.threads_request_flags

--- src/debugProtocol/events/requests/ThreadsRequest.ts
+++ src/debugProtocol/events/requests/ThreadsRequest.ts
@@ -5,9 +5,12 @@ import type { ProtocolRequest } from '../ProtocolEvent';
 
 export class ThreadsRequest implements ProtocolRequest {
 
-    public static fromJson(data: { requestId: number }) {
+    public static fromJson(data: { requestId: number, threads_request_flags?: number }) {
         const request = new ThreadsRequest();
         protocolUtil.loadJson(request, data);
+        if (data.threads_request_flags !== undefined) {
+            request.data.threads_request_flags = data.threads_request_flags;
+        }
         return request;
     }
 
@@ -15,12 +18,18 @@ export class ThreadsRequest implements ProtocolRequest {
         const request = new ThreadsRequest();
         protocolUtil.bufferLoaderHelper(request, buffer, 12, (smartBuffer) => {
             protocolUtil.loadCommonRequestFields(request, smartBuffer);
+            if (smartBuffer.remaining() >= 4) {
+                request.data.threads_request_flags = smartBuffer.readUInt32LE();
+            }
         });
         return request;
     }
 
     public toBuffer(): Buffer {
         const smartBuffer = new SmartBuffer();
+        if (this.data.threads_request_flags !== undefined) {
+            smartBuffer.writeUInt32LE(this.data.threads_request_flags);
+        }
         protocolUtil.insertCommonRequestFields(this, smartBuffer);
         return smartBuffer.toBuffer();
     }
@@ -31,10 +40,18 @@ export class ThreadsRequest implements ProtocolRequest {
      */
     public readOffset: number = undefined;
 
-    public data = {
-        //common props
+    public data: {
+        packetLength: number;
+        requestId: number;
+        command: Command;
+        threads_request_flags?: number; 
+    } = {
         packetLength: undefined as number,
         requestId: undefined as number,
-        command: Command.Threads
+        command: Command.Threads,
     };
+}
+
+export enum ThreadRequestFlags {
+    includeThreadInfo = 0x0001
 }
